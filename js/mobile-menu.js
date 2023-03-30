1acedf78a6e5d317e61d74510dc4f08eb402fa22
VladM <vdstories20@gmail.com>
2023-03-31 00:41:22 +0200

    fix responsive layout for all devices on both pages

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -21,7 +21,7 @@
   closeMenuBtn.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(max-width: 481px)').addEventListener('change', e => {
+  window.matchMedia('(min-width: 481px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
